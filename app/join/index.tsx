c54fbb4dcff140d9266abcf5dbb0bd5de7840d3b
Tim <tim_vanlerberg@science.edu>
2024-09-11 12:50:02 -0500

    Github Auth

--- app/join/index.tsx
+++ app/join/index.tsx
@@ -1,15 +1,55 @@
-import { Alert, Platform, View } from 'react-native'
-import * as AppleAuthentication from 'expo-apple-authentication'
-import supabase from '@/api/Supabase'
-import { useQueryClient } from '@tanstack/react-query'
-import Session from '@/api/queries/Session'
+import Session from "@/api/queries/Session";
+import supabase from "@/api/Supabase";
+import { useQueryClient } from "@tanstack/react-query";
+import * as AppleAuthentication from "expo-apple-authentication";
+import { makeRedirectUri } from "expo-auth-session";
+import * as QueryParams from "expo-auth-session/build/QueryParams";
+import * as WebBrowser from "expo-web-browser";
+import { Alert, Button, Platform, View } from "react-native";
+
+WebBrowser.maybeCompleteAuthSession(); // required for web only
+const redirectTo = makeRedirectUri();
+
+const createSessionFromUrl = async (url: string) => {
+  const { params, errorCode } = QueryParams.getQueryParams(url);
+
+  if (errorCode) throw new Error(errorCode);
+  const { access_token, refresh_token } = params;
+
+  if (!access_token) return;
+
+  const { data, error } = await supabase.auth.setSession({
+    access_token,
+    refresh_token,
+  });
+  if (error) throw error;
+  return data.session;
+};
+
+const performOAuth = async () => {
+  const { data, error } = await supabase.auth.signInWithOAuth({
+    provider: "github",
+    options: {
+      redirectTo,
+      skipBrowserRedirect: true,
+    },
+  });
+  if (error) throw error;
+
+  const res = await WebBrowser.openAuthSessionAsync(data?.url ?? "", redirectTo);
+
+  if (res.type === "success") {
+    const { url } = res;
+    await createSessionFromUrl(url);
+  }
+};
 
 export default function Auth() {
-  const qc = useQueryClient()
-  if (Platform.OS === 'ios')
+  const qc = useQueryClient();
+  if (Platform.OS === "ios")
     return (
-      <View style={{ alignItems: 'center', justifyContent: 'center', flex: 1 }}>
-        {/* <Button title="Sign in with Github" onPress={signInWithGithub} /> */}
+      <View style={{ alignItems: "center", justifyContent: "center", flex: 1 }}>
+        <Button title="Sign in with Github" onPress={performOAuth} />
         <AppleAuthentication.AppleAuthenticationButton
           buttonType={AppleAuthentication.AppleAuthenticationButtonType.SIGN_IN}
           buttonStyle={AppleAuthentication.AppleAuthenticationButtonStyle.BLACK}
@@ -22,20 +62,20 @@ export default function Auth() {
                   AppleAuthentication.AppleAuthenticationScope.FULL_NAME,
                   AppleAuthentication.AppleAuthenticationScope.EMAIL,
                 ],
-              })
+              });
               // Sign in via Supabase Auth.
               if (credential.identityToken) {
                 const { error } = await supabase.auth.signInWithIdToken({
-                  provider: 'apple',
+                  provider: "apple",
                   token: credential.identityToken,
-                })
-                if (error) Alert.alert(error?.message)
-                if (!error) qc.invalidateQueries({ queryKey: Session.queryOptions.queryKey })
+                });
+                if (error) Alert.alert(error?.message);
+                if (!error) qc.invalidateQueries({ queryKey: Session.queryOptions.queryKey });
               } else {
-                throw new Error('No identityToken.')
+                throw new Error("No identityToken.");
               }
             } catch (e: any) {
-              if (e.code === 'ERR_REQUEST_CANCELED') {
+              if (e.code === "ERR_REQUEST_CANCELED") {
                 // handle that the user canceled the sign-in flow
               } else {
                 // handle other errors
@@ -44,6 +84,6 @@ export default function Auth() {
           }}
         />
       </View>
-    )
-  return <>{/* Implement Android Auth options. */}</>
+    );
+  return <>{/* Implement Android Auth options. */}</>;
 }
